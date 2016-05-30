ea21f54c1ff866684222617607b5b104d79b61c8
Phil Flanagan <phileflanagan@gmail.com>
2016-05-30 14:01:40 -0400

    deploy to heroku

---
+++ client.js
@@ -0,0 +1,7 @@
+module.exports = {
+  consumer_key:         'N42oIZv9zgzE0zZ91dK4gpyIE',
+  consumer_secret:      'SmrRpbIKWwR1ngCswePG8zlH84znMmKxTW5tYNXfAueH0MdB9A',
+  access_token:         '737187156070924288-8gpHHyI95SBhTPsnuJL4MbFMrLAdVb0',
+  access_token_secret:  'RjyTnJDkg25Y9z0bDfjKAbyZ2x7BaBIwbismNaQwc2FbF',
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+}
